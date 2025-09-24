96a462dbc6fc1107c6714082bcd57f5416f196c7
anshuraj0904 <rajanshu0904@gmail.com>
2025-09-24 21:29:07 +0530

    Updated the treatment model and added the payment part

--- backend/models/treatement.model.js
+++ backend/models/treatement.model.js
@@ -1,14 +1,30 @@
 import mongoose from "mongoose";
 
-
 const treatmentSchema = new mongoose.Schema({
-    appointmentId :{
+    appointmentId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref:"Appointment"
+        ref: "Appointment",
+        required: true
     },
+    statement: {
+        type: String,
+        required: true
+    },
+    prescription: [String],
+    billAmount: {
+        type: Number,
+        required: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user", // doctor
+        required: true
+    },
+    paymentStatus: {
+        type: String,
+        enum: ["yet to pay", "paid"],
+        default: "yet to pay"
+    }
+}, { timestamps: true });
 
-    statement : {type:String},
-    prescription: [String]
-})
-
-export default mongoose.model("Treatment", treatmentSchema)
+export default mongoose.model("Treatment", treatmentSchema);
